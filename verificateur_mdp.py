baad33a628e5184cb482aa16fd60671a4d8b26bb
Van <92119774+mvsteven@users.noreply.github.com>
2022-08-21 16:53:50 +0200

    Update verificateur_mdp.py

--- verificateur_mdp.py
+++ verificateur_mdp.py
@@ -1,12 +1,12 @@
 mdp = input("Entrez un mot de passe (min 8 caractères) : ")
 mdp_trop_court = "votre mot de passe est trop court."
 
-
 if mdp.isdigit():
     print("Votre mot de passe ne contient que des nombres.")
-elif mdp.isdigit() == False:
+else:
     if len(mdp) != 0 and len(mdp) < 8:
         mdp_trop_court = mdp_trop_court.capitalize()
+        print(mdp_trop_court)
     elif len(mdp) == 0:
         mdp_trop_court = mdp_trop_court.upper()
         print(mdp_trop_court)
